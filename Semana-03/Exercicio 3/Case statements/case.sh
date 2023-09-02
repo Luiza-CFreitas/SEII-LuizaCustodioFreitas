3cce21b1bc434ffe61715d74f02c68d73722d468
Luiza Custódio Freitas <128931229+Luiza-CFreitas@users.noreply.github.com>
2023-09-02 14:01:37 -0300

    Add files via upload

---
+++ Semana-03/Exercicio 3/Case statements/case.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+case $1 in
+    Luiza | luiza)
+        echo "Oh, you're the boss here. Welcome!"
+        ;;
+    *)
+        echo "Hello there, but you are not the boss of me! Please enter a valid username!"
+        ;;
+esac
